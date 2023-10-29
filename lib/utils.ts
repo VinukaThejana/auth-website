0c1f343eb6df247979d3c7e9bb587ecedd527b51
Vinuka Kodituwakku <vinuka.t@pm.me>
2023-10-29 13:21:52 +0530

    Moved auth/register -> register and auth/login -> login

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,6 +1,14 @@
-import { type ClassValue, clsx } from "clsx"
-import { twMerge } from "tailwind-merge"
- 
+import { type ClassValue, clsx } from "clsx";
+import { twMerge } from "tailwind-merge";
+
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
+}
+
+export function getCookie(name: string) {
+  function escape(s: string) {
+    return s.replace(/([.*+?\^$(){}|\[\]\/\\])/g, "\\$1");
+  }
+  var match = document.cookie.match(RegExp("(?:^|;\\s*)" + escape(name) + "=([^;]*)"));
+  return match ? match[1] : null;
 }
