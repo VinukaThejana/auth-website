99284503ef90e7dd5f510bb57e937833e4dcbc1c
Vinuka Kodituwakku <vinuka.t@pm.me>
2023-11-05 17:05:01 +0530

    Formalized the layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,35 +1,37 @@
-import type { Metadata } from 'next'
-import { Inter as FontSans } from "next/font/google"
-import { Toaster } from 'sonner'
-import './globals.css'
-import { cn } from '~/lib/utils'
+import type { Metadata } from "next";
+import { Inter as FontSans } from "next/font/google";
+import { Toaster } from "sonner";
+import "./globals.css";
+import { Navbar } from "~/components/util/navbar";
+import { cn } from "~/lib/utils";
 
 const fontSans = FontSans({
   subsets: ["latin"],
   variable: "--font-sans",
-})
+});
 
 export const metadata: Metadata = {
-  title: 'Auth Frontend',
-  description: 'Frontend user authentication',
-}
+  title: "Auth Frontend",
+  description: "Frontend user authentication",
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
       <body
         className={cn(
           "min-h-screen bg-background font-sans antialiased",
-          fontSans.variable
+          fontSans.variable,
         )}
       >
+        <Navbar />
         <Toaster />
         {children}
       </body>
     </html>
-  )
+  );
 }
