10d4171768bc71ea81dff9f90c7cb8fcf482b16b
Vinuka Kodituwakku <vinuka.t@pm.me>
2023-11-10 17:52:16 +0530

    Added some helper functions to convert between some formats

--- lib/utils.ts
+++ lib/utils.ts
@@ -12,3 +12,22 @@ export function getCookie(name: string) {
   var match = document.cookie.match(RegExp("(?:^|;\\s*)" + escape(name) + "=([^;]*)"));
   return match ? match[1] : null;
 }
+
+export const HOST_SETTINGS = {
+  expectedOrigin: process.env.VERCEL_URL ?? "http://localhost:3000",
+  expectedRPID: process.env.RPID ?? "localhost",
+};
+
+export function binaryToBase64url(bytes: Uint8Array) {
+  let str = "";
+
+  bytes.forEach((charCode) => {
+    str += String.fromCharCode(charCode);
+  });
+
+  return btoa(str);
+}
+
+export function clean(str: string) {
+  return str.replace(/\+/g, "-").replace(/\//g, "_").replace(/=/g, "");
+}
