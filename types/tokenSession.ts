a28bb0d416656d79c012adc0d1ff82bc8c82d898
Vinuka Kodituwakku <vinuka.t@pm.me>
2023-11-20 23:49:58 +0530

    Added the sesion token type

---
+++ types/tokenSession.ts
@@ -0,0 +1,10 @@
+export type SessionToken = {
+  ID: string;
+  UserID: string;
+  LoginAt: string;
+  IPAddress: string;
+  Location: string;
+  Device: string;
+  OS: string;
+  ExpiresAt: number;
+};
