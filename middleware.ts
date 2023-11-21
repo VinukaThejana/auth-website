13223411ea9df53201abe85361b56564835e47f5
Vinuka Kodituwakku <vinuka.t@pm.me>
2023-11-21 22:12:59 +0530

    Send the user agnet details as a cookie to the client side

--- middleware.ts
+++ middleware.ts
@@ -1,22 +1,48 @@
-import { NextRequest, NextResponse } from "next/server";
+import { base64url } from "jose";
+import { NextRequest, NextResponse, userAgent } from "next/server";
 import { isLoggedIn } from "./lib/auth";
 
 export async function middleware(request: NextRequest) {
   const path = request.nextUrl.pathname;
   const loggedIn = isLoggedIn(request);
+  const { device, os } = userAgent(request);
 
   switch (path) {
     case "/":
       if (!loggedIn) {
         return NextResponse.rewrite(new URL("/login", request.url));
       }
+      break;
     case "/login":
       if (loggedIn) {
         return NextResponse.rewrite(new URL("/", request.url));
       }
+      break;
   }
 
-  return NextResponse.next();
+  const ua = {
+    device: {
+      vendor: device.vendor ?? "",
+      model: device.model ?? "",
+    },
+    os: {
+      name: os.name ?? "",
+      version: os.version ?? "",
+    },
+  };
+
+  const response = NextResponse.next();
+  if (request.cookies.get("ua") === undefined) {
+    response.cookies.set({
+      name: "ua",
+      value: base64url.encode(JSON.stringify(ua)),
+      path: "/",
+      maxAge: 10 * 60,
+      httpOnly: false,
+    });
+  }
+
+  return response;
 }
 
 export const config = {
