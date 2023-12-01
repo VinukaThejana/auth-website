cfd28df31b7d386d357c3e1cc83bda5fae8e390d
Vinuka Kodituwakku <vinuka.t@pm.me>
2023-12-01 13:17:17 +0530

    Removed sonner

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from "next";
-import { Inter as FontSans, Prompt } from "next/font/google";
-import { Toaster } from "sonner";
+import { Inter as FontSans } from "next/font/google";
+import { Toaster } from "~/components/ui/toaster";
 import "./globals.css";
 import { Navbar } from "~/components/util/navbar";
 import { cn } from "~/lib/utils";
