81c85f7d09a232f1f0e3b7f1dbd530c5299c7308
Vinuka Kodituwakku <vinuka.t@pm.me>
2023-12-07 09:33:42 +0530

    Added the oauth component for managing all the oauth providers

---
+++ components/auth/oauth.tsx
@@ -0,0 +1,27 @@
+"use client";
+
+import { Button } from "~/components/ui/button";
+import { Icons } from "~/components/ui/icons";
+import { BACKEND_URL } from "~/lib/utils";
+
+export function OAuthProviders(props: {
+  isLoading: boolean;
+}) {
+  const { isLoading } = props;
+  return (
+    <>
+      <Button
+        onClick={async () => {
+          window.location.href = `${BACKEND_URL}/oauth/github/redirect`;
+        }}
+        variant="outline"
+        type="button"
+        disabled={isLoading}
+      >
+        {isLoading
+          ? <Icons.spinner className="mr-2 h-4 w-4 animate-spin" />
+          : <Icons.gitHub className="mr-2 h-4 w-4" />} Github
+      </Button>
+    </>
+  );
+}
