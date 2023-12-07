4433bec51f97968c9da3098f0e5735cc9f74cd78
Vinuka Kodituwakku <vinuka.t@pm.me>
2023-12-07 09:32:38 +0530

    Added the backend url

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,6 +1,8 @@
 import { type ClassValue, clsx } from "clsx";
 import { twMerge } from "tailwind-merge";
 
+export const BACKEND_URL = "http://localhost:8080";
+
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
