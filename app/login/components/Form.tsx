d84868821034c39b68bc32b001a86659bb945e8f
Vinuka Kodituwakku <vinuka.t@pm.me>
2023-12-09 21:02:11 +0530

    Added a state manager to handle oauth related issues

--- app/login/components/Form.tsx
+++ app/login/components/Form.tsx
@@ -2,8 +2,7 @@
 
 import { zodResolver } from "@hookform/resolvers/zod";
 import { AxiosError } from "axios";
-import { useRouter } from "next/navigation";
-import * as React from "react";
+import { useRouter, useSearchParams } from "next/navigation";
 import { useForm } from "react-hook-form";
 import { BsLock, BsUnlock } from "react-icons/bs";
 import { z } from "zod";
@@ -20,18 +19,47 @@ import { cn } from "~/lib/utils";
 import { Errs } from "~/types/errors";
 import { User } from "~/types/user";
 import { schema } from "../utils/schema";
+import { Dialog, Transition } from '@headlessui/react'
+import { AddNewUsernameOAuth } from "~/components/auth/new-username-modal";
+import { HTMLAttributes, useEffect, useState } from "react";
 
-interface UserAuthFormProps extends React.HTMLAttributes<HTMLDivElement> {}
+interface UserAuthFormProps extends HTMLAttributes<HTMLDivElement> { }
 
 export function LoginForm({ className, ...props }: UserAuthFormProps) {
+  const searchParams = useSearchParams();
+  const state = searchParams.get('state');
+
   const router = useRouter();
   const { toast } = useToast();
 
-  const [isPasswordVisible, setPasswordVisible] = React.useState(false);
-  const [isLoading, setIsLoading] = React.useState<boolean>(false);
-  const [user, setUser] = React.useState<User | null>(null);
-  const [twoFactorEnabled, setTwoFactorEnabled] = React.useState(false);
-  const [code, setCode] = React.useState("");
+  const [isPasswordVisible, setPasswordVisible] = useState(false);
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+  const [user, setUser] = useState<User | null>(null);
+  const [twoFactorEnabled, setTwoFactorEnabled] = useState(false);
+  const [code, setCode] = useState("");
+  const [open, setOpen] = useState(false);
+
+  useEffect(() => {
+    if (state !== null) {
+      switch (state as Errs) {
+        case "add_a_username":
+          setOpen(true);
+          break;
+        case "unauthorized":
+          toast({
+            title: "Failed",
+            description: "Failed to connect the account"
+          })
+          break;
+        default:
+          toast({
+            title: "Failed",
+            description: "Something went wrong",
+          })
+      }
+    }
+    // eslint-disable-next-line
+  }, [state])
 
   const { register, handleSubmit, formState, reset } = useForm<z.infer<typeof schema>>({
     resolver: zodResolver(schema),
@@ -88,146 +116,153 @@ export function LoginForm({ className, ...props }: UserAuthFormProps) {
   }
 
   return (
-    <div className={cn("grid gap-4", className)} {...props}>
-      <>
-        {twoFactorEnabled
-          ? (
-            <div className="grid gap-4">
-              <div className="grid gap-2">
-                <Label className="" htmlFor="code">
-                  Enter the code
-                </Label>
-                <Input
-                  id="code"
-                  placeholder="123-456"
-                  type="text"
-                  autoCapitalize="none"
-                  value={code}
-                  onChange={(e) => setCode(e.currentTarget.value)}
-                />
-                <Button
-                  onClick={async () => {
-                    if (!user || code === "") {
-                      toast({
-                        title: "Error",
-                        description: "Something went wrong",
-                      });
-                      return;
-                    }
-                    try {
-                      await authApi.post("/otp/validate", {
-                        "id": user.id,
-                        "code": code,
-                      });
-                      setCode("");
-                      setUser(null);
-                      setIsLoading(false);
-
-                      router.push("/");
-                    } catch (error) {
-                      const err = error as AxiosError<{
-                        status: Errs;
-                      }>;
-                      switch (err.response?.data.status) {
-                        case "otp_token_is_not_valid":
-                          toast({
-                            title: "Invalid OTP code",
-                            description: "The OTP token that you provided is not valid",
-                          });
-                          break;
-                        case "two_factor_verification_not_enabled":
-                          toast({
-                            title: "Two factor authentication is not enabled",
-                            description:
-                              "Two factor authentication is not enabled please enable two factor authentication and try again",
-                          });
-                          break;
-                        default:
-                          toast({
-                            title: "Error",
-                            description: "Something went wrong",
-                          });
-                      }
-                    }
-                  }}
-                >
-                  Login
-                </Button>
-              </div>
-            </div>
-          )
-          : (
-            <form onSubmit={handleSubmit(onSubmit)}>
+    <>
+      <div className={cn("grid gap-4", className)} {...props}>
+        <>
+          {twoFactorEnabled
+            ? (
               <div className="grid gap-4">
                 <div className="grid gap-2">
-                  <Label className="" htmlFor="username">
-                    Username
+                  <Label className="" htmlFor="code">
+                    Enter the code
                   </Label>
-                  <div className="grid gap-2">
-                    <Input
-                      id="username"
-                      placeholder="JohnDoe"
-                      type="text"
-                      autoCapitalize="none"
-                      autoComplete="username webauthn"
-                      autoCorrect="off"
-                      disabled={isLoading}
-                      {...register("username")}
-                    />
-                  </div>
-                  <FormError err={errors.username} />
+                  <Input
+                    id="code"
+                    placeholder="123-456"
+                    type="text"
+                    autoCapitalize="none"
+                    value={code}
+                    onChange={(e) => setCode(e.currentTarget.value)}
+                  />
+                  <Button
+                    onClick={async () => {
+                      if (!user || code === "") {
+                        toast({
+                          title: "Error",
+                          description: "Something went wrong",
+                        });
+                        return;
+                      }
+                      try {
+                        await authApi.post("/otp/validate", {
+                          "id": user.id,
+                          "code": code,
+                        });
+                        setCode("");
+                        setUser(null);
+                        setIsLoading(false);
+
+                        router.push("/");
+                      } catch (error) {
+                        const err = error as AxiosError<{
+                          status: Errs;
+                        }>;
+                        switch (err.response?.data.status) {
+                          case "otp_token_is_not_valid":
+                            toast({
+                              title: "Invalid OTP code",
+                              description: "The OTP token that you provided is not valid",
+                            });
+                            break;
+                          case "two_factor_verification_not_enabled":
+                            toast({
+                              title: "Two factor authentication is not enabled",
+                              description:
+                                "Two factor authentication is not enabled please enable two factor authentication and try again",
+                            });
+                            break;
+                          default:
+                            toast({
+                              title: "Error",
+                              description: "Something went wrong",
+                            });
+                        }
+                      }
+                    }}
+                  >
+                    Login
+                  </Button>
                 </div>
-                <div className="grid gap-2">
-                  <Label className="" htmlFor="password">
-                    Password
-                  </Label>
-                  <div className="grid grid-cols-5 gap-2">
-                    <Input
-                      id="password"
-                      type={isPasswordVisible ? "text" : "password"}
-                      autoCapitalize="none"
-                      autoComplete="password"
-                      autoCorrect="off"
-                      disabled={isLoading}
-                      className="col-span-4"
-                      {...register("password")}
-                    />
-                    <span
-                      className="border border-slate-200 rounded-lg inline-flex items-center justify-center col-span-1"
-                      onClick={() => setPasswordVisible(!isPasswordVisible)}
-                    >
-                      {isPasswordVisible ? <BsUnlock /> : <BsLock />}
-                    </span>
-                  </div>
-                  <FormError err={errors.password} />
-                </div>
-                <Button
-                  type="submit"
-                  disabled={isLoading || !formState.isValid}
-                >
-                  {isLoading && <Icons.spinner className="mr-2 h-4 w-4 animate-spin" />}
-                  Sign In with Email
-                </Button>
               </div>
-            </form>
-          )}
-      </>
+            )
+            : (
+              <form onSubmit={handleSubmit(onSubmit)}>
+                <div className="grid gap-4">
+                  <div className="grid gap-2">
+                    <Label className="" htmlFor="username">
+                      Username
+                    </Label>
+                    <div className="grid gap-2">
+                      <Input
+                        id="username"
+                        placeholder="JohnDoe"
+                        type="text"
+                        autoCapitalize="none"
+                        autoComplete="username webauthn"
+                        autoCorrect="off"
+                        disabled={isLoading}
+                        {...register("username")}
+                      />
+                    </div>
+                    <FormError err={errors.username} />
+                  </div>
+                  <div className="grid gap-2">
+                    <Label className="" htmlFor="password">
+                      Password
+                    </Label>
+                    <div className="grid grid-cols-5 gap-2">
+                      <Input
+                        id="password"
+                        type={isPasswordVisible ? "text" : "password"}
+                        autoCapitalize="none"
+                        autoComplete="password"
+                        autoCorrect="off"
+                        disabled={isLoading}
+                        className="col-span-4"
+                        {...register("password")}
+                      />
+                      <span
+                        className="border border-slate-200 rounded-lg inline-flex items-center justify-center col-span-1"
+                        onClick={() => setPasswordVisible(!isPasswordVisible)}
+                      >
+                        {isPasswordVisible ? <BsUnlock /> : <BsLock />}
+                      </span>
+                    </div>
+                    <FormError err={errors.password} />
+                  </div>
+                  <Button
+                    type="submit"
+                    disabled={isLoading || !formState.isValid}
+                  >
+                    {isLoading && <Icons.spinner className="mr-2 h-4 w-4 animate-spin" />}
+                    Sign In with Email
+                  </Button>
+                </div>
+              </form>
+            )}
+        </>
 
-      <PassKeys />
+        <PassKeys />
 
-      <div className="relative">
-        <div className="absolute inset-0 flex items-center">
-          <span className="w-full border-t" />
+        <div className="relative">
+          <div className="absolute inset-0 flex items-center">
+            <span className="w-full border-t" />
+          </div>
+          <div className="relative flex justify-center text-xs uppercase">
+            <span className="bg-background px-2 text-muted-foreground">
+              Or continue with
+            </span>
+          </div>
         </div>
-        <div className="relative flex justify-center text-xs uppercase">
-          <span className="bg-background px-2 text-muted-foreground">
-            Or continue with
-          </span>
-        </div>
+        <OAuthProviders
+          isLoading={isLoading}
+        />
       </div>
-      <OAuthProviders
-        isLoading={isLoading}
+
+      <AddNewUsernameOAuth
+        open={open}
+        setOpen={setOpen}
       />
-    </div>
+    </>
   );
 }
