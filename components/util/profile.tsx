e3aaf07f06e3b2445bca7b6ec97f9a648e990d43
Vinuka Kodituwakku <vinuka.t@pm.me>
2023-12-19 23:12:04 +0530

    Added a react query to fetch the user details

--- components/util/profile.tsx
+++ components/util/profile.tsx
@@ -1,6 +1,8 @@
 "use client";
 
+import { useQuery } from "@tanstack/react-query";
 import { AxiosError } from "axios";
+import Image from "next/image";
 import { useRouter } from "next/navigation";
 import { authApi, checkAccessToken } from "~/lib/api";
 import { getUser } from "~/lib/user";
@@ -9,39 +11,55 @@ import { Button } from "../ui/button";
 import { useToast } from "../ui/use-toast";
 
 export default function Profile() {
-  const user = getUser();
+  const { data: user, status, refetch } = useQuery({
+    queryKey: ["user"],
+    queryFn: () => getUser(),
+  });
   const router = useRouter();
   const { toast } = useToast();
 
-  return user
-    ? (
-      <img
-        src={user.photo_url}
-        alt={user.name}
-        className="w-10 rounded-full"
-        onClick={async () => {
-          try {
-            authApi.interceptors.request.use(checkAccessToken);
-            await authApi.delete("/logout");
-            router.push("/login");
-          } catch (error) {
-            const err = error as AxiosError<{
-              status: Errs;
-            }>;
-            console.error(err.response?.data.status);
-            toast({
-              title: "Failed",
-              description: "Something went wrong",
-            });
-          }
-        }}
-      />
-    )
-    : (
-      <Button
-        onClick={() => router.push("/login")}
-      >
-        Login
-      </Button>
-    );
+  return (
+    <>
+      {status === "success"
+        ? (
+          <>
+            {user
+              ? (
+                <Image
+                  src={user.photo_url}
+                  alt={user.name}
+                  className="w-10 rounded-full"
+                  width={100}
+                  height={100}
+                  onClick={async () => {
+                    try {
+                      authApi.interceptors.request.use(checkAccessToken);
+                      await authApi.delete("/logout");
+                      await refetch();
+                      router.push("/login");
+                    } catch (error) {
+                      const err = error as AxiosError<{
+                        status: Errs;
+                      }>;
+                      console.error(err.response?.data.status);
+                      toast({
+                        title: "Failed",
+                        description: "Something went wrong",
+                      });
+                    }
+                  }}
+                />
+              )
+              : (
+                <Button
+                  onClick={() => router.push("/login")}
+                >
+                  Login
+                </Button>
+              )}
+          </>
+        )
+        : null}
+    </>
+  );
 }
