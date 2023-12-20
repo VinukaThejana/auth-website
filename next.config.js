06c5ad84989e9334442917fdbc611b8bab06f8f8
Vinuka Kodituwakku <vinuka.t@pm.me>
2023-12-20 23:11:14 +0530

    Added localhost as an allowed domain for displaying next image

--- next.config.js
+++ next.config.js
@@ -11,6 +11,10 @@ const nextConfig = {
         protocol: "https",
         hostname: "api.dicebear.com",
       },
+      {
+        protocol: "http",
+        hostname: "localhost"
+      }
     ],
   },
 };
